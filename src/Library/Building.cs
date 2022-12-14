51d1cb9f4b13a0dedc01e28d5c20bdb6b866a0a1
Gusti <gusti@hotmail.com>
2022-12-14 11:32:46 -0300

    ConsoleCs

---
+++ src/Library/Building.cs
@@ -0,0 +1,57 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Building
+    {
+        private ArrayList tasks = new ArrayList();
+
+        public Building(string name)
+        {
+            this.Description = name;
+        }
+
+        public string Description { get; set; }
+
+        public void AddTask(Task task)
+        {
+            this.tasks.Add(task);
+        }
+
+        public void RemoveTask(Task task)
+        {
+            this.tasks.Remove(task);
+        }
+
+        public void PrintBuilding()
+        {
+            Console.WriteLine($"Edificio {this.Description}:");
+            foreach (Task task in this.tasks)
+            {
+                Console.WriteLine($"{task.Quantity} de '{task.Material.Description}' " +
+                    $"usando '{task.Equipment.Description}' durante {task.Time}");
+            }
+        }
+        
+        public double GetProductionCost()
+        {
+            double costoMateriales = 0;
+            double costoHerramientas = 0;
+            foreach (Task task in this.tasks)
+            {
+                costoMateriales += task.Material.UnitCost * task.Quantity;
+                costoHerramientas += task.Equipment.HourlyCost * task.Time;
+            }
+            double totalCost = costoMateriales + costoHerramientas;
+            return totalCost;
+        }
+        
+    }
+}
